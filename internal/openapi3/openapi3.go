fbca3956e864169164e809837c5f16a679dcf9b2
Alexander Melentyev <alexander@melentyev.org>
2021-11-12 16:35:59 +0300

    feature: Add fields description

--- internal/openapi3/openapi3.go
+++ internal/openapi3/openapi3.go
@@ -2,6 +2,8 @@ package openapi3
 
 // The root document object of the OpenAPI document.
 type OpenAPI struct {
+	// This string MUST be the semantic version number of the OpenAPI Specification version that the OpenAPI document uses. The openapi field SHOULD be used by tooling specifications and clients to interpret the OpenAPI document. This is not related to the API info.version string.
 	OpenAPI string `json:"openapi" yaml:"openapi"`
-	Info    *Info  `json:"info" yaml:"info"`
+	// Provides metadata about the API. The metadata MAY be used by tooling as required.
+	Info *Info `json:"info" yaml:"info"`
 }
