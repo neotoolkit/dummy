551626213a9871f273adff6ae96508e2837aa04f
Alexander Melentyev <alexander@melentyev.org>
2021-11-17 23:22:54 +0300

    feature: Add exit codes

--- internal/command/server.go
+++ internal/command/server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/go-dummy/dummy/internal/exitcode"
 	"github.com/go-dummy/dummy/internal/openapi3"
 	"github.com/go-dummy/dummy/internal/server"
 )
@@ -25,7 +26,7 @@ func (e *Executor) initServer() {
 func (e *Executor) executeServer(_ *cobra.Command, args []string) {
 	if len(args) == 0 {
 		fmt.Fprint(os.Stderr, "empty path\n")
-		os.Exit(3)
+		os.Exit(exitcode.Failure)
 	}
 
 	e.cfg.Server.Path = args[0]
@@ -33,13 +34,13 @@ func (e *Executor) executeServer(_ *cobra.Command, args []string) {
 	openapi, err := openapi3.Parse(e.cfg.Server.Path)
 	if err != nil {
 		fmt.Fprint(os.Stderr, "can't parse yaml\n")
-		os.Exit(3)
+		os.Exit(exitcode.Failure)
 	}
 
 	s := server.NewServer(e.cfg.Server, openapi)
 
 	if err := s.Run(); err != nil {
 		fmt.Fprint(os.Stderr, "server error\n")
-		os.Exit(3)
+		os.Exit(exitcode.Failure)
 	}
 }
