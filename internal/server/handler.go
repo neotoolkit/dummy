ad2635efed348c8734f922adc0632f031d67b510
Alexander Melentyev <alexander@melentyev.org>
2021-11-21 23:07:01 +0300

    refactor: Check first symbol

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -190,7 +190,7 @@ func pathMaskDetect(path, mask string) bool {
 	}
 
 	for i := 0; i < len(p); i++ {
-		if strings.HasPrefix(m[i], "{") {
+		if len(m[i]) > 0 && m[i][0] == '{' {
 			continue
 		}
 
