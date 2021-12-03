99342ba5d6329da79aa608920e417b87e647758f
Alexander Melentyev <alexander@melentyev.org>
2021-12-03 23:13:09 +0300

    Add more OpenAPI fields and small refactoring

--- internal/openapi3/parameter.go
+++ internal/openapi3/parameter.go
@@ -1,8 +1,10 @@
 package openapi3
 
 type Parameter struct {
-	Name string `json:"name,omitempty" yaml:"name,omitempty"`
-	In   string `json:"in,omitempty" yaml:"in,omitempty"`
+	Name     string  `json:"name,omitempty" yaml:"name,omitempty"`
+	In       string  `json:"in,omitempty" yaml:"in,omitempty"`
+	Required bool    `json:"required,omitempty" yaml:"required,omitempty"`
+	Schema   *Schema `json:"schema,omitempty" yaml:"schema,omitempty"`
 }
 
 type Parameters []Parameter
