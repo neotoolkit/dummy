98d56a05baaa1bb76b1d58b3f2b68c45e34fd026
Sasha Melentyev <sasha@melentyev.io>
2022-01-03 11:12:49 +0300

    refactor(apischema): Refactor fuzz test

--- internal/apischema/find_test.go
+++ internal/apischema/find_test.go
@@ -12,10 +12,36 @@ import (
 )
 
 func FuzzPathByParamDetect(f *testing.F) {
-	f.Add("", "", true)
-	f.Add("/path", "/path", true)
-	f.Add("/path/1", "/path/{1}", true)
-	f.Add("/path/1/path/2", "/path/{1}/path/{2}", true)
+	tests := []struct {
+		path  string
+		param string
+		want  bool
+	}{
+		{
+			path:  "",
+			param: "",
+			want:  true,
+		},
+		{
+			path:  "/path",
+			param: "/path",
+			want:  true,
+		},
+		{
+			path:  "/path/1",
+			param: "/path/{1}",
+			want:  true,
+		},
+		{
+			path:  "/path/1/path/2",
+			param: "/path/{1}/path/{2}",
+			want:  true,
+		},
+	}
+
+	for _, seed := range tests {
+		f.Add(seed.path, seed.param, seed.want)
+	}
 
 	f.Fuzz(func(t *testing.T, path, param string, want bool) {
 		got := apischema.PathByParamDetect(path, param)
