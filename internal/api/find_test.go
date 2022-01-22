7058ab5e1725bf006f57e40f3e4ed631bf6a2fd7
Sasha Melentyev <sasha@melentyev.io>
2022-01-22 15:57:29 +0300

    test(api): add PathByParamDetect case

--- internal/api/find_test.go
+++ internal/api/find_test.go
@@ -45,6 +45,12 @@ func TestPathByParamDetect(t *testing.T) {
 			param: "/path/{1}/path/{2}",
 			want:  false,
 		},
+		{
+			name:  "",
+			path:  "/path/1/path1/2",
+			param: "/path/{1}/path/{2}",
+			want:  false,
+		},
 	}
 
 	for _, tc := range tests {
