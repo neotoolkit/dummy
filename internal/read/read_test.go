2daa3adb1881b20e2ee36464790aa1c945c0a195
Sasha Melentyev <sasha@melentyev.io>
2022-01-23 18:43:16 +0300

    test(read): add more tests

--- internal/read/read_test.go
+++ internal/read/read_test.go
@@ -2,6 +2,11 @@ package read_test
 
 import (
 	"errors"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,24 +15,72 @@ import (
 )
 
 func TestRead(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, `{
+   "openapi": "3.0.3",
+   "info": {
+      "title": "Test dummy API",
+      "version": "0.1.0"
+   },
+   "paths": {
+      "/healthz": {
+         "get": {
+            "responses": {
+               "201": {
+                  "description": ""
+               }
+            }
+         }
+      }
+   }
+}`)
+	}))
+	defer ts.Close()
+
 	tests := []struct {
 		name string
 		path string
 		err  error
 	}{
 		{
-			name: "read file",
+			name: "empty URL",
+			path: "https://",
+			err: &url.Error{
+				Op:  "Get",
+				URL: "https:",
+				Err: errors.New("http: no Host in request URL"),
+			},
+		},
+		{
+			name: "read from URL",
+			path: ts.URL,
+			err:  nil,
+		},
+		{
+			name: "read from file",
 			path: "./testdata/openapi3.yml",
 			err:  nil,
+		},
+		{
+			name: "empty file",
+			path: "",
+			err: &fs.PathError{
+				Op:   "open",
+				Path: "",
+				Err:  errors.New("no such file or directory"),
+			},
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			got, err := read.Read(tc.path)
+			if err != nil {
+				require.EqualError(t, err, tc.err.Error())
+			}
 
 			require.IsType(t, []byte{}, got)
-			require.True(t, errors.Is(err, tc.err))
 		})
 	}
 }
