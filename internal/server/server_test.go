57d20dfc4f520280aec921918f412e68eea6f9de
Sasha Melentyev <sasha@melentyev.io>
2022-02-14 20:12:01 +0300

    test(server): add server test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,15 +1,34 @@
 package server_test
 
 import (
+	"context"
+	"errors"
 	"github.com/neotoolkit/dummy/internal/config"
 	"github.com/neotoolkit/dummy/internal/logger"
 	"github.com/neotoolkit/dummy/internal/server"
 	"github.com/stretchr/testify/require"
+	"net/http"
 	"testing"
+	"time"
 )
 
 func TestNewServer(t *testing.T) {
 	s := server.NewServer(config.Server{}, &logger.Logger{}, server.Handlers{})
 
 	require.IsType(t, &server.Server{}, s)
+}
+
+func TestNewServer_Run(t *testing.T) {
+	l := logger.NewLogger("DEBUG")
+
+	s := server.NewServer(config.Server{}, l, server.Handlers{})
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		s.Stop(context.Background())
+	}()
+
+	err := s.Run()
+
+	require.True(t, errors.Is(err, http.ErrServerClosed))
 }
