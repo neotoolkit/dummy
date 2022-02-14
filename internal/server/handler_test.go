02fc2723d24e9ddb61411160568347dbfd8e5386
Sasha Melentyev <sasha@melentyev.io>
2022-02-14 20:37:02 +0300

    test(server): add handlers test

--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -13,3 +13,13 @@ func TestNewHandlers(t *testing.T) {
 
 	require.IsType(t, server.Handlers{}, h)
 }
+
+func TestNewHandlers_Get(t *testing.T) {
+	h := server.NewHandlers(api.API{}, &logger.Logger{})
+
+	resp, ok, err := h.Get("", "", nil)
+
+	require.Equal(t, api.Response{}, resp)
+	require.Equal(t, false, ok)
+	require.NotNil(t, err)
+}
